398bb56843fe691056982a39bb87c2187161f1e1
8061-maker <8061@crms5denha.ac.th>
2026-01-08 09:40:06 +0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+let nubber = prompt("อายุเท่าไหร่ : ")
+if(nubber > 12){
+    document.getElementById("content").innerHTML = "ไอ่แบงค์ดกย์จริงๆ"
+
+    document.getElementById("bank").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/VbSM7pKaH28?si=SPYW8pVIqefSBuh5\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+}
